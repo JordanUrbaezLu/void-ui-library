27578652f725e30695e4adda6afbd39adf00c00a
Jordan Urbaez Lu <93447469+JordanUrbaezLu@users.noreply.github.com>
2023-01-12 22:56:33 -0500

    feat(AstroLink): add AstroLink component (#18)

---
+++ src/components/AstroLink/AstroLink.stories.tsx
@@ -0,0 +1,25 @@
+import { Story } from "@storybook/react";
+import AstroLink, { AstroLinkProps } from "./AstroLink";
+
+export default {
+  component: AstroLink,
+  title: "Components/AstroLink",
+};
+
+const Template: Story<Omit<AstroLinkProps, "children" | "href">> = (args) => {
+  return (
+    <AstroLink children="Google.com" href="https://www.google.com/" {...args} />
+  );
+};
+
+export const Default = Template.bind({});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: "primary",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: "secondary",
+};
