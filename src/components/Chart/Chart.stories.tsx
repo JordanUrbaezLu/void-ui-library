323fde31de47081cc65c823f826f09feaaf9a5ab
Jordan Urbaez Lu <jordana.urbaez@gmail.com>
2023-02-17 09:57:23 -0500

    feat(Chart): fix Chromatic snapshot for Chart

--- src/components/Chart/Chart.stories.tsx
+++ src/components/Chart/Chart.stories.tsx
@@ -2,37 +2,35 @@ import { Chart } from "./Chart";
 
 export default {
   component: Chart,
+  // Delay Chromatic capture to allow animation for Chart
+  parameters: { chromatic: { delay: 2000 } },
   title: "Components/Chart",
-};
-
-const getRandomInteger = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const data = [
   {
     name: "2018",
-    Angular: getRandomInteger(4000, 5000),
-    React: getRandomInteger(1000, 2000),
-    Vue: getRandomInteger(0, 1000),
+    Angular: 4500,
+    React: 1000,
+    Vue: 200,
   },
   {
     name: "2019",
-    Angular: getRandomInteger(4000, 5000),
-    React: getRandomInteger(3000, 4000),
-    Vue: getRandomInteger(500, 1500),
+    Angular: 4000,
+    React: 3000,
+    Vue: 1200,
   },
   {
     name: "2020",
-    Angular: getRandomInteger(2000, 3000),
-    React: getRandomInteger(4000, 5000),
-    Vue: getRandomInteger(2000, 3000),
+    Angular: 2800,
+    React: 4400,
+    Vue: 2700,
   },
   {
     name: "2021",
-    Angular: getRandomInteger(1000, 2000),
-    React: getRandomInteger(5000, 6000),
-    Vue: getRandomInteger(4000, 5000),
+    Angular: 2000,
+    React: 5600,
+    Vue: 3600,
   },
 ];
 
